e5ac3f575e6390e25d733e0ce3d612c73030ced1
AgustinMadygraf <agustin.mtto.madygraf>
2025-01-26 04:22:48 -0300

    Update .gitignore, refactor app_flask.py to remove UrlService instantiation, and enhance config-example.json with model selection

--- app_flask.py
+++ app_flask.py
@@ -4,7 +4,6 @@ Path: app_flask.py
 import sys
 from core.config.flask_config import FlaskConfig
 from core.data_controller import data_controller
-from core.services.url_service import UrlService
 
 # Inicializar la configuración de Flask
 flask_config = FlaskConfig()
@@ -19,19 +18,7 @@ except RuntimeError as e:
     flask_config.logger.error("Error al registrar el blueprint: %s", e)
     sys.exit(1)
 
-# Crear instancia del servicio
-url_service = UrlService(config['ENDPOINT_NGROK_PHP'])
-
 if __name__ == '__main__':
-    # print("Starting Flask server...")
-    # try:
-    #     url = url_service.get_public_url()
-    #     url_service.save_url(url)
-    #     logger.info("URL enviada correctamente.")
-    # except (ConnectionError, ValueError) as e:
-    #     logger.error("Error al ejecutar el servicio de URL: %s", e)
-
-    # Finalmente iniciar Flask
     try:
         app.run(debug=config['IS_DEVELOPMENT'], host='0.0.0.0', port=5000)
         flask_config.logger.info("Servidor configurado para HTTP.")
